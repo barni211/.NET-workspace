66acd6c0e5a183569e8c8cec76d6ab4af313a223
barni211 <barni211@gmail.com>
2017-12-28 12:50:03 +0100

    Rename Visitator to visitor

--- ClientToOwnServer/ClientToOwnServer/Program.cs
+++ ClientToOwnServer/ClientToOwnServer/Program.cs
@@ -13,62 +13,68 @@ namespace ClientToServer
         private static Logger.Logger log;
         static void Main(string[] args)
         {
-            int num = 143;
-            createLogger();
-            TcpClient socketForServer;
-            try
+            for (int i = 0; i < 10; i++)
             {
-                socketForServer = new TcpClient("localHost", 1800);
-            }
-            catch
-            {
-                log.WriteLog(Level.ERROR,
-                "Failed to connect to server at {0}:999" + "localhost");
-                return;
-            }
-
-            NetworkStream networkStream = socketForServer.GetStream();
-            System.IO.StreamReader streamReader =
-            new System.IO.StreamReader(networkStream);
-            System.IO.StreamWriter streamWriter =
-            new System.IO.StreamWriter(networkStream);
-            log.WriteLog(Level.INFO, "*******This is client program who is connected to localhost on port No:10*****");
-
-            try
-            {
-                string outputString;
-                // read the data from the host and display it
+                int num = 143;
+                createLogger();
+                TcpClient socketForServer;
+                try
                 {
-                    //outputString = streamReader.ReadLine();
-                    //Console.WriteLine("Message Recieved by server:" + outputString);
 
-                    //Console.WriteLine("Type your message to be recieved by server:");
-                    Console.WriteLine("type:");
-                    string str = Console.ReadLine();
-                    while (str != "exit")
-                    {
-                        streamWriter.WriteLine(str);
-                        streamWriter.Flush();
-                        Console.WriteLine("type:");
-                        str = Console.ReadLine();
-                    }
-                    if (str == "exit")
-                    {
-                        streamWriter.WriteLine(str);
-                        streamWriter.Flush();
-
-                    }
-
+                    socketForServer = new TcpClient("localHost", 1800);
                 }
+                catch
+                {
+                    log.WriteLog(Level.ERROR,
+                    "Failed to connect to server at {0}:999" + "localhost");
+                    return;
+                }
+
+                NetworkStream networkStream = socketForServer.GetStream();
+                System.IO.StreamReader streamReader =
+                new System.IO.StreamReader(networkStream);
+                System.IO.StreamWriter streamWriter =
+                new System.IO.StreamWriter(networkStream);
+                log.WriteLog(Level.INFO, "*******This is client program who is connected to localhost on port No:10*****");
+
+                try
+                {
+                    string outputString;
+                    // read the data from the host and display it
+                    {
+                        //outputString = streamReader.ReadLine();
+                        //Console.WriteLine("Message Recieved by server:" + outputString);
+
+                        //Console.WriteLine("Type your message to be recieved by server:");
+                        Console.WriteLine("type:");
+                        string str = Console.ReadLine();
+                        while (str != "exit")
+                        {
+                            streamWriter.WriteLine(str);
+                            streamWriter.Flush();
+                            Console.WriteLine("type:");
+                            str = Console.ReadLine();
+                        }
+                        if (str == "exit")
+                        {
+                            streamWriter.WriteLine(str);
+                            streamWriter.Flush();
+
+                        }
+
+                    }
+                }
+                catch
+                {
+                    log.WriteLog(Level.ERROR, "Exception reading from Server");
+                }
+
+                // tidy up
+                networkStream.Close();
             }
-            catch
-            {
-                log.WriteLog(Level.ERROR, "Exception reading from Server");
-            }
-            // tidy up
-            networkStream.Close();
             Console.WriteLine("Press any key to exit from client program");
             Console.ReadKey();
+
             
         }
 
---
+++ Programowanie-uslug-sieciowych-klient/Programowanie-uslug-sieciowych-klient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programowanie_uslug_sieciowych_klient
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Client frmClient = new Client();
+            frmClient.ShowDialog();
+        }
+    }
+}
